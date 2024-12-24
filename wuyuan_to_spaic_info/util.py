d55975a950c6fa6733ee79447d666f8b79750aee
mdstm <shdaed@163.com>
2024-12-24 14:27:54 +0800

    删除旧文件

--- wuyuan_to_spaic_info/util.py
+++
@@ -1,9 +0,0 @@
-def update(d: dict, d1: dict):
-  '''递归更新字典'''
-  for k, v1 in d1.items():
-    v = d.get(k)
-    if isinstance(v, dict) and isinstance(v1, dict):
-      update(v, v1)
-    else:
-      d[k] = v1
-  return d
